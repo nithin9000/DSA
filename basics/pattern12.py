a5d791bd7907ad34ca3b457b7dcfee7a3c1d66ba
nithin9000 <nithinkrishnalal303@gmail.com>
2025-06-01 21:27:28 +0530

    pattern

--- basics/pattern12.py
+++ basics/pattern12.py
@@ -1,5 +1,23 @@
+'''
+n=6
+
+1          1
+12        21
+123      321
+1234    4321
+12345  54321
+123456654321
+'''
+
 n = int(input())
 for i in range(1,n+1):
-	for j in range(i):
-		print(j+1,end="")
+	for j in range(1,i+1):
+		print(j,end="")
+
+	space = 2*(n-i)
+	for _ in range(space):
+		print(" ",end = "")
+
+	for j in range(i,0,-1):
+		print(j,end="")
 	print()
